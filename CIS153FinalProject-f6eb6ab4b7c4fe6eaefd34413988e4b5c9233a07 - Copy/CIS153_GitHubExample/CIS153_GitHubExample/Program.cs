f8ac18813cb0944f83a0bd566800cc22f7452594
MattCassady <102937612+MattCassady@users.noreply.github.com>
2022-04-25 01:42:56 -0400

    Update Program.cs

--- CIS153FinalProject-f6eb6ab4b7c4fe6eaefd34413988e4b5c9233a07 - Copy/CIS153_GitHubExample/CIS153_GitHubExample/Program.cs
+++ CIS153FinalProject-f6eb6ab4b7c4fe6eaefd34413988e4b5c9233a07 - Copy/CIS153_GitHubExample/CIS153_GitHubExample/Program.cs
@@ -16,7 +16,7 @@ namespace final_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CIS153_GitHubExample.Main_Menu());
         }
     }
 }
